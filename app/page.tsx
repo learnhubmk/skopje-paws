a4ac1a6150489c872dd4a09c5a47fd0fcf83ac1e
mariocvetanoski <cvetanoski.mario92@gmail.com>
2024-06-24 20:29:46 +0200

    initial Steps commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import Hero from "../components/Hero";
 import GalleryPage from "../components/Gallery";
 import Form from "@/Form";
+import Steps from "@/Steps/Steps";
 
 
 export default function Home() {
     return (
         <div>
-            <Hero/>
-            <GalleryPage folderName={`Showcase`}/>
-            <GalleryPage folderName={`Services`}/>
-            <Form/>
+            <Hero />
+            <Steps />
+            <GalleryPage folderName={`Showcase`} />
+            <GalleryPage folderName={`Services`} />
+            <Form />
         </div>
     )
 }
---
+++ components/Steps/StepsCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StepsCard = () => {
+    return (
+        <div>StepsCard</div>
+    )
+}
+
+export default StepsCard
