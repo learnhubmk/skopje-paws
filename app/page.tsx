6b3dc7d5b124496ad8bc48d9135d44666cd63a5e
Daniel <daniel.kochovski12@gmail.com>
2024-06-28 19:08:03 +0200

    created 3 dropdowns

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "../components/Hero";
 import GalleryPage from "../components/Gallery";
 import Form from "@/Form";
+import FAQDropDown from "../components/FAQ/FAQDropdown";
 
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
             <GalleryPage folderName={`Showcase`}/>
             <GalleryPage folderName={`Services`}/>
             <Form/>
+            <FAQDropDown />
         </div>
     )
 }
