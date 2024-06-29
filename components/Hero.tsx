20fb53be5704cf542c80f2f992d324a243755fac
SmartW0rker <hristijanchochevski@gmail.com>
2024-06-29 12:45:41 +0200

    Hero section: Initial commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,19 @@
 import TextImage from "./TextImage";
-import HeroImage from "../public/favicon.jpg";
+import HeroImage from "../public/hero.svg";
 
 const Hero = () => {
     return (
-        <TextImage image={HeroImage} text={`Hero Section`} style={`flex flex-col justify-between items-center h-full overflow-hidden w-full mt-10`}></TextImage>
-    );
+        <div>
+            <TextImage image={HeroImage} text={`Hero Section`}
+                       style={`relative flex flex-col justify-between items-center px-12 h-full overflow-hidden w-full mt-10`}></TextImage>
+            <div className="absolute top-1/3 text-start text-black " style={{ marginLeft: '78px' }}>
+                <p className="text-5xl font-bold">Ние ќе го прошетаме</p>
+                <p className="text-5xl">вашето милениче</p>
+            </div>
+        </div>
+
+)
+    ;
 };
 
 export default Hero;
--- components/TextImage.tsx
+++ components/TextImage.tsx
@@ -11,7 +11,7 @@ const TextImage = ({image, text, style}: TextImageProps) => {
     return (
         <div className={style}>
            <p className={`mb-4`}> {text} </p>
-            <Image src={image} alt={`hero image`}  priority/>
+            <Image src={image} alt={`hero image`} className={`w-full`} priority/>
         </div>
 
     )
