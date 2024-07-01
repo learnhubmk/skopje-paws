dbe440862249b57482df479518cb91f99259f330
Daniel <daniel.kochovski12@gmail.com>
2024-07-01 18:17:18 +0200

    changed the div size to smaller, and centered it, and changed fonts sizes and colors following the figma design

--- components/FAQ/FAQAccordion.tsx
+++ components/FAQ/FAQAccordion.tsx
@@ -4,12 +4,12 @@ import React from 'react'
 
 export default function FAQAccordion() {
   return (
-    <div className="grid grid-cols-2 gap-10 p-10">
+    <div className="grid grid-cols-2 gap-10 p-10 w-4/5" style={{ margin: '0 auto' }}>
 
-      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'start' }}>
-        <h2>Често поставувани
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'start', gap: '20px' }}>
+        <h2 className='text-5xl font-bold'>Често поставувани
           прашања</h2>
-        <p>Погледнете дали вашето прашање се наоѓа во овие прашања и договори. Доколку не се наоѓа, слобдно контактирајте не преку копчето подолу.</p>
+        <p className='font-medium text-xl'>Погледнете дали вашето прашање се наоѓа во овие прашања и договори. Доколку не се наоѓа, слобдно контактирајте не преку копчето подолу.</p>
         <LinkButton type={'link'} url={''} text={'Поставете прашање'} bgColor={'bg-orange'} textColor={'text-black'}></LinkButton>
       </div>
       {/* accordions */}
@@ -18,7 +18,7 @@ export default function FAQAccordion() {
           <summary
             className="flex cursor-pointer items-center justify-between gap-1.5 rounded-lg bg-gray-50 p-4 text-black-900" style={{ border: '1px solid #FFAC66', borderRadius: '56px' }}
           >
-            <h2 className="font-medium">Како да закажам прошетка?</h2>
+            <h2 className='font-medium text-xl'>Како да закажам прошетка?</h2>
 
             <svg
               className="size-5 shrink-0 transition duration-300 group-open:-rotate-180"
@@ -31,7 +31,7 @@ export default function FAQAccordion() {
             </svg>
           </summary>
 
-          <p className="mt-4 p-5 leading-relaxed text-gray-700" style={{ border: '1px solid #FFAC66', borderRadius: '25px', background: '#FFAC66' }}>
+          <p className="mt-4 p-5 leading-relaxed font-medium text-xl" style={{ border: '1px solid #FFAC66', borderRadius: '25px', background: '#FFAC66' }}>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis molestias culpa in,
             recusandae laboriosam neque aliquid libero nesciunt voluptate dicta quo officiis explicabo
             consequuntur distinctio corporis earum similique!
@@ -41,7 +41,7 @@ export default function FAQAccordion() {
         <details className="group [&_summary::-webkit-details-marker]:hidden">
           <summary
             className="flex cursor-pointer items-center justify-between gap-1.5 rounded-lg bg-gray-50 p-4 text-black-900" style={{ border: '1px solid #FFAC66', borderRadius: '56px' }}>
-            <h2 className="font-medium">Како оди процесот на запознавање?</h2>
+            <h2 className="font-medium text-xl">Како оди процесот на запознавање?</h2>
 
             <svg
               className="size-5 shrink-0 transition duration-300 group-open:-rotate-180"
@@ -54,7 +54,7 @@ export default function FAQAccordion() {
             </svg>
           </summary>
 
-          <p className="mt-4 p-5 leading-relaxed text-black-700" style={{ border: '1px solid #FFAC66', borderRadius: '25px', background: '#FFAC66' }}>
+          <p className="mt-4 p-5 leading-relaxed font-medium text-xl" style={{ border: '1px solid #FFAC66', borderRadius: '25px', background: '#FFAC66' }}>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis molestias culpa in,
             recusandae laboriosam neque aliquid libero nesciunt voluptate dicta quo officiis explicabo
             consequuntur distinctio corporis earum similique!
@@ -63,9 +63,9 @@ export default function FAQAccordion() {
 
         <details className="group [&_summary::-webkit-details-marker]:hidden">
           <summary
-            className="flex cursor-pointer items-center justify-between gap-1.5 rounded-lg bg-gray-50 p-4 text-black-900" style={{ border: '1px solid #FFAC66', borderRadius: '56px' }}
+            className="flex cursor-pointer items-center justify-between gap-1.5 rounded-lg bg-gray-50 p-4 font-medium text-xl" style={{ border: '1px solid #FFAC66', borderRadius: '56px' }}
           >
-            <h2 className="font-medium">Од што зависи цената на прошетката?</h2>
+            <h2 className="font-medium text-xl">Од што зависи цената на прошетката?</h2>
 
             <svg
               className="size-5 shrink-0 transition duration-300 group-open:-rotate-180"
@@ -78,7 +78,7 @@ export default function FAQAccordion() {
             </svg>
           </summary>
 
-          <p className="mt-4 p-5 leading-relaxed text-black-700" style={{ border: '1px solid #FFAC66', borderRadius: '25px', background: '#FFAC66' }}>
+          <p className="mt-4 p-5 leading-relaxed font-medium text-xl" style={{ border: '1px solid #FFAC66', borderRadius: '25px', background: '#FFAC66' }}>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis molestias culpa in,
             recusandae laboriosam neque aliquid libero nesciunt voluptate dicta quo officiis explicabo
             consequuntur distinctio corporis earum similique!
