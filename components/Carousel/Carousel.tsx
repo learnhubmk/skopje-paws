b322ef668addf4dc94e8a4816d0e9ae6ae5e0eb4
mariocvetanoski <cvetanoski.mario92@gmail.com>
2024-07-05 12:05:34 +0200

    added different pictures and fixed responsiveness

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React from 'react';
-import Image from 'next/image';
 import SlideCard from './SlideCard';
 import { EmblaOptionsType } from 'embla-carousel';
 import {
@@ -17,6 +16,7 @@ type PropType = {
 };
 
 const EmblaCarousel = (props: PropType) => {
+    const slideNames = ['Арија', 'Реа', 'Фреја', 'Јоги', 'Дона'];
     const { slides, options } = props;
     const [emblaRef, emblaApi] = useEmblaCarousel(options);
     const {
@@ -25,18 +25,19 @@ const EmblaCarousel = (props: PropType) => {
         onPrevButtonClick,
         onNextButtonClick
     } = usePrevNextButtons(emblaApi);
-    console.log(slides)
+
     return (
-        <section className="w-11/12 mx-auto max-w-screen-2xl">
-            <div className="flex justify-end gap-3 mt-7">
+        <section className="w-10/12 lg:w-11/12 mx-auto max-w-screen-2xl">
+            <div className="flex justify-between items-center gap-3 mb-11 mt-14">
+                <div className='text-black font-bold text-2xl sm:text-5xl'>Нашите среќи</div>
                 <div className="grid grid-cols-2 gap-1.5 items-center">
                     <PrevButton
-                        className="cursor-pointer w-12 h-12 z-10 text-black flex items-center justify-center"
+                        className="cursor-pointer w-8 h-8 sm:w-12 sm:h-12 z-10 text-black flex items-center justify-center"
                         onClick={onPrevButtonClick}
                         disabled={prevBtnDisabled}
                     />
                     <NextButton
-                        className="cursor-pointer w-12 h-12 z-10 text-black flex items-center justify-center"
+                        className="cursor-pointer w-8 h-8 sm:w-12 sm:h-12 z-10 text-black flex items-center justify-center"
                         onClick={onNextButtonClick}
                         disabled={nextBtnDisabled}
                     />
@@ -44,8 +45,8 @@ const EmblaCarousel = (props: PropType) => {
             </div>
             <div className="overflow-hidden" ref={emblaRef}>
                 <div className="flex touch-pan-y">
-                    {slides.map((index) => (
-                        <SlideCard key={index} index={index} />
+                    {slideNames.map((name, index) => (
+                        <SlideCard key={index} name={name} index={index} />
                     ))}
                 </div>
             </div>
--- components/Carousel/SlideCard.tsx
+++ components/Carousel/SlideCard.tsx
@@ -5,25 +5,26 @@ import Image from 'next/image';
 
 type SlideCardProps = {
     index: number;
+    name: string;
 };
 
-const SlideCard = ({ index }: SlideCardProps) => {
+const SlideCard = ({ index, name }: SlideCardProps) => {
     return (
         <div
-            className={`flex-0 flex-shrink-0 basis-[70%] max-w-[400px] lg:max-w-[500px] xl:max-w-[500px] min-w-0 ${index > 0 ? '-ml-6' : 'pl-2'} relative`}
+            className={`rounded-3xl border-orange border-4 flex-0 flex-shrink-0 basis-[70%] max-w-[400px] lg:max-w-[500px] xl:max-w-[500px] min-w-0 ${index > 0 ? '-ml-6' : '-ml-4'} relative`}
             style={{ zIndex: 5 + index }}
         >
-            <div className="relative rounded-3xl overflow-hidden h-[600px] border-4 border-orange">
+            <div className="relative rounded-3xl overflow-hidden sm:h-[600px] min-h-[300px]">
                 <Image
-                    src={`/images/slide-${index + 1}.jpg`} // Ensure your path is correct
+                    src={`/images/slide-${index + 1}.jpg`}
                     alt={`Slide ${index + 1}`}
                     layout="fill"
                     objectFit="cover"
                 />
             </div>
-            {/* Optional: You can overlay content on top of the image if needed */}
-            <div className="absolute inset-0 flex items-center justify-center">
-                <div className="text-white text-4xl font-semibold">Slide {index + 1}</div>
+            <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-50 rounded-3xl"></div>
+            <div className="absolute inset-0 flex items-end justify-center mb-5">
+                <div className="text-white text-4xl font-semibold">{name}</div>
             </div>
         </div>
     );
