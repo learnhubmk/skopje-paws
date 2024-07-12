6382fc7399f9a155fdf1dbc3175413acfafd9b80
mariocvetanoski <cvetanoski.mario92@gmail.com>
2024-07-12 14:35:02 +0200

    added ; for consistensy

--- components/Carousel/CarouselButtons.tsx
+++ components/Carousel/CarouselButtons.tsx
@@ -5,64 +5,62 @@ import React, {
     useCallback,
     useEffect,
     useState
-} from 'react'
-import { EmblaCarouselType } from 'embla-carousel'
+} from 'react';
+import { EmblaCarouselType } from 'embla-carousel';
 
 type UsePrevNextButtonsType = {
     prevBtnDisabled: boolean
     nextBtnDisabled: boolean
     onPrevButtonClick: () => void
     onNextButtonClick: () => void
-}
+};
 
 export const usePrevNextButtons = (
     emblaApi: EmblaCarouselType | undefined
 ): UsePrevNextButtonsType => {
-    const [prevBtnDisabled, setPrevBtnDisabled] = useState(true)
-    const [nextBtnDisabled, setNextBtnDisabled] = useState(true)
+    const [prevBtnDisabled, setPrevBtnDisabled] = useState(true);
+    const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
 
     const onPrevButtonClick = useCallback(() => {
         if (!emblaApi) return
         emblaApi.scrollPrev()
-    }, [emblaApi])
+    }, [emblaApi]);
 
     const onNextButtonClick = useCallback(() => {
         if (!emblaApi) return
         emblaApi.scrollNext()
-    }, [emblaApi])
+    }, [emblaApi]);
 
     const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
         setPrevBtnDisabled(!emblaApi.canScrollPrev())
         setNextBtnDisabled(!emblaApi.canScrollNext())
-    }, [])
+    }, []);
 
     useEffect(() => {
         if (!emblaApi) return
 
         onSelect(emblaApi)
         emblaApi.on('reInit', onSelect).on('select', onSelect)
-    }, [emblaApi, onSelect])
+    }, [emblaApi, onSelect]);
 
     return {
         prevBtnDisabled,
         nextBtnDisabled,
         onPrevButtonClick,
         onNextButtonClick
-    }
-}
+    };
+};
 
 type ButtonPropType = ComponentPropsWithRef<'button'> & {
     direction: 'prev' | 'next'
-}
+};
 
 const CarouselButton = ({ direction, ...restProps }: ButtonPropType) => {
     const isPrev = direction === 'prev'
     const buttonClass = `embla__button embla__button--${direction}`
     const svgPath = isPrev ?
         "M355.66 11.354c13.793-13.805 36.208-13.805 50.001 0 13.785 13.804 13.785 36.238 0 50.034L201.22 266l204.442 204.61c13.785 13.805 13.785 36.239 0 50.044-13.793 13.796-36.208 13.796-50.002 0a5994246.277 5994246.277 0 0 0-229.332-229.454 35.065 35.065 0 0 1-10.326-25.126c0-9.2 3.393-18.26 10.326-25.2C172.192 194.973 332.731 34.31 355.66 11.354Z"
-        : "M176.34 520.646c-13.793 13.805-36.208 13.805-50.001 0-13.785-13.804-13.785-36.238 0-50.034L330.78 266 126.34 61.391c-13.785-13.805-13.785-36.239 0-50.044 13.793-13.796 36.208-13.796 50.002 0 22.928 22.947 206.395 206.507 229.332 229.454a35.065 35.065 0 0 1 10.326 25.126c0 9.2-3.393 18.26-10.326 25.2-45.865 45.901-206.404 206.564-229.332 229.52Z"
-
-
+        : "M176.34 520.646c-13.793 13.805-36.208 13.805-50.001 0-13.785-13.804-13.785-36.238 0-50.034L330.78 266 126.34 61.391c-13.785-13.805-13.785-36.239 0-50.044 13.793-13.796 36.208-13.796 50.002 0 22.928 22.947 206.395 206.507 229.332 229.454a35.065 35.065 0 0 1 10.326 25.126c0 9.2-3.393 18.26-10.326 25.2-45.865 45.901-206.404 206.564-229.332 229.52Z";
     return (
         <button
             className={buttonClass}
@@ -77,12 +75,12 @@ const CarouselButton = ({ direction, ...restProps }: ButtonPropType) => {
             </svg>
         </button>
     )
-}
+};
 
 export const PrevButton = (props: ComponentPropsWithRef<'button'>) => (
     <CarouselButton direction='prev' {...props} />
-)
+);
 
 export const NextButton = (props: ComponentPropsWithRef<'button'>) => (
     <CarouselButton direction='next' {...props} />
-)
+);
