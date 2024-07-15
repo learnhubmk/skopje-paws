2333a5f556b76ce324f43e60b7f2b379130f8ce4
Daniel <daniel.kochovski12@gmail.com>
2024-07-15 21:26:40 +0200

    aligned answer text with header and stopped arrow from moving to the right

--- components/FAQ/FAQAccordion.tsx
+++ components/FAQ/FAQAccordion.tsx
@@ -12,7 +12,7 @@ export default function FAQAccordion({ question, answer, isActive }) {
           <svg className={`transition-all duration-300 ${isActive ? 'rotate-180' : 'fill-orange'}`} xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M12 17.414 3.293 8.707l1.414-1.414L12 14.586l7.293-7.293 1.414 1.414L12 17.414z" /></svg>
         </h2>
 
-        <p className={`transition-all duration-300 mt-4 font-medium text-xl rounded-3xl ${isActive ? 'h-auto p-5' : 'h-0'} ${montserrat.className}`}>
+        <p className={`transition-all duration-300 mt-4 font-medium text-xl rounded-3xl ${isActive ? 'h-auto' : 'h-0'} ${montserrat.className}`}>
           {isActive && answer}
         </p>
       </div>
