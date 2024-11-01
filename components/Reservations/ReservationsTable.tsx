235c0549e9af21b40d81c0ec8783ee05c35abaaf
maznevskia <andrej.maznevski13@gmail.com>
2024-11-01 15:18:20 +0100

    Add Pagination to Reservations table

--- components/Reservations/ReservationsTable.tsx
+++ components/Reservations/ReservationsTable.tsx
@@ -142,6 +142,73 @@ export default function ReactTable({ reservations }) {
                         )}
                     </tbody>
                 </table>
+                <div className="flex items-center gap-2">
+                    <button
+                        className="border rounded p-1"
+                        onClick={() => table.firstPage()}
+                        disabled={!table.getCanPreviousPage()}
+                    >
+                        {'<<'}
+                    </button>
+                    <button
+                        className="border rounded p-1"
+                        onClick={() => table.previousPage()}
+                        disabled={!table.getCanPreviousPage()}
+                    >
+                        {'<'}
+                    </button>
+                    <button
+                        className="border rounded p-1"
+                        onClick={() => table.nextPage()}
+                        disabled={!table.getCanNextPage()}
+                    >
+                        {'>'}
+                    </button>
+                    <button
+                        className="border rounded p-1"
+                        onClick={() => table.lastPage()}
+                        disabled={!table.getCanNextPage()}
+                    >
+                        {'>>'}
+                    </button>
+                    <span className="flex items-center gap-1">
+                        <div>Страна</div>
+                        <strong>
+                            {table.getState().pagination.pageIndex + 1} од {' '}
+                            {table.getPageCount().toLocaleString()}
+                        </strong>
+                    </span>
+                    <span className="flex items-center gap-1">
+                        | Оди до страна:
+                        <input
+                            type="number"
+                            min="1"
+                            max={table.getPageCount()}
+                            defaultValue={table.getState().pagination.pageIndex + 1}
+                            onChange={e => {
+                                const page = e.target.value ? Number(e.target.value) - 1 : 0
+                                table.setPageIndex(page)
+                            }}
+                            className="border p-1 rounded w-16"
+                        />
+                    </span>
+                    <select
+                        value={table.getState().pagination.pageSize}
+                        onChange={e => {
+                            table.setPageSize(Number(e.target.value))
+                        }}
+                    >
+                        {[10, 20, 30, 40, 50].map(pageSize => (
+                            <option key={pageSize} value={pageSize}>
+                                Покажи {pageSize}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div>
+                    Прикажани {table.getRowModel().rows.length.toLocaleString()} од {' '}
+                    {table.getRowCount().toLocaleString()} Резервации
+                </div>
             </div>
         </div>
     );
