6d704870396b6ab09a7405d1ff227bd3983db161
maznevskia <andrej.maznevski13@gmail.com>
2024-11-27 18:14:10 +0100

    Fix page misalignment

--- components/Calendar/ReservationComponent.tsx
+++ components/Calendar/ReservationComponent.tsx
@@ -219,7 +219,7 @@ export default function ReservationComponent() {
 
     return (
         <div id="reservation" className={`${montserrat.className} flex items-center justify-center font-sans py-8 px-2 text-charcoal`}>
-            <div className="flex flex-col lg:flex-row items-center justify-center rounded-3xl relative w-full p-10 sm:p-0 gap-8">
+            <div className="flex flex-col lg:flex-row items-center justify-center rounded-3xl relative w-full p-0 gap-8">
                 <Calendar
                     minDate={startOfTomorrow()}
                     onClickDay={handleDateClick}
